36677eda52aa39c0297339ce24f7373545cd3b26
ricardodarocha <ricardodarocha@outlook.com>
2019-09-19 14:42:43 -0300

    removida unit ugeneralIcons

--- pCalc.dpr
+++ pCalc.dpr
@@ -4,7 +4,6 @@ uses
   Vcl.Forms,
   uCalc in 'uCalc.pas' {frmCalc},
   JvExprParser in 'JvExprParser.pas',
-  uGeneralIcon in '..\telas\uGeneralIcon.pas' {CNV: TDataModule},
   uFuncoes in 'uFuncoes.pas',
   uVariaveis in 'uVariaveis.pas',
   assis.strings in 'assis.strings.pas';
@@ -15,6 +14,5 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmCalc, frmCalc);
-  Application.CreateForm(TCNV, CNV);
   Application.Run;
 end.
